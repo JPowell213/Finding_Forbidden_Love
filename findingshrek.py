8edcb723f15cc112e6cb0de8488cda140b61e657
unknown <jedijohnathan@gmail.com>
2015-03-06 12:28:49 -0800

    Jp did the no side of the introduction

--- findingshrek.py
+++ findingshrek.py
@@ -26,20 +26,23 @@ def intro():
         intro()
 
 ################ JP's Work #####################
-def choice1():
+def choice2():
     choice = simpledialog.askinteger("Choose wisely",
                                      "You didn't buy an onion and you go into the garage to put some groceries away and you find an onion.  Do you pick it up? 1 or 2.")
     if (choice == 1):
         messagebox.showinfo("Yes",
-                            "You pick up the onion and go for a drive to think, as your driving you see the batmobile fly right next to you and \n.  THE END")
+                            "You pick up the onion and go for a drive to think, as your driving you see the batmobile fly right next to you and n\
+you see a batarange get knocked out of the air as if it hit something and you live on as if nothing happened.  THE END")
     elif (choice == 2):
         messagebox.showinfo("The End",
-                            "You chose ok.  THE END")
+                            "You didn't pick up the onion. You go on a drive to think about something that n\
+has been happening at your job, and suddenly you hear a roar on an engine as you see the batmobile come out of no where and fly n\
+right next to you then you see an explosive batarang hit your car and blow up your car...THE END")
     else:
         choice1()
 
 ################ Gordon's Work #####################
-def choice2():
+def choice1():
     choice = simpledialog.askinteger("Choose wisely",
                                      "This is the next part of the story.  Now you must choose 1 or 2 again.")
     if (choice == 1):
